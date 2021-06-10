cf43aed63013415baa0b7862ab954f3da6d09272
qadubrovin <qadubrovin@yandex.ru>
2021-06-10 20:02:04 +0300

    changing armstrong

--- src/test/java/Armstrong2000.java
+++ src/test/java/Armstrong2000.java
@@ -1,5 +1,8 @@
 public class Armstrong2000 {
     public static void main(String[] args) {
         System.out.println("Im Loui Armstrong!");
+        System.out.println("But i changed my sex");
+        System.out.println("And now im  a helicopter!");
+        System.out.println("Im Armstrong2000 now!");
     }
 }
